45d8b462fc0f56298123b83b67989ed1aaea2f93
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-10-21 09:46:38 -0500

    creating HelpApp to run spring boot

---
+++ src/main/java/help/HelpApp.java
@@ -0,0 +1,13 @@
+package help;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpApp.class, args);
+    }
+
+}
