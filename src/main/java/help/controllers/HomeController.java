71ef4746fef903d8960053e965286114235b9494
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-10-21 16:15:51 -0500

    created test package and updated example.properties

--- src/main/java/help/controllers/HomeController.java
+++ src/main/java/help/controllers/HomeController.java
@@ -1,4 +1,13 @@
 package help.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "This is the landing page!";
+    }
 }
