98ea8df170bbb7585071de15a518e357128de3a9
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-10-22 11:58:13 -0500

    user register and login created

--- src/main/java/help/controllers/UserController.java
+++ src/main/java/help/controllers/UserController.java
@@ -1,11 +1,30 @@
 package help.controllers;
 
+import help.models.User;
 import help.repositories.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class UserController {
     private final UserRepository userDao;
 
     public UserController(UserRepository userDao) {
         this.userDao = userDao;
     }
+
+    @GetMapping("/register")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/register";
+    }
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user){
+        userDao.save(user);
+        return "redirect:/login";
+    }
+
 }
