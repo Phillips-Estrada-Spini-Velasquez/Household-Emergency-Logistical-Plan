1ee9445e11c440fa91e3101f70f8c66dab6349f3
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-11-08 00:10:27 -0600

    completed map destination functionality! :D

--- src/main/resources/static/js/keys.js
+++ src/main/resources/static/js/keys.js
@@ -1,5 +1,6 @@
 // MAPBOX API KEY
 const mapboxKey = "pk.eyJ1IjoiZHNwaW5pIiwiYSI6ImNrZ21hZnZwZjAxaG0ycmxtYXVsMGhsdDgifQ.UYpZcU3JQ1oBFpWjLkzo7w";
 
-// FILESTACK API KEY
-const filestackKey = "AOv9JDwjT5GbgYz2ExCuQz";
+// DB details
+const dbUsername = "root";
+const dbPassword = "codeup";
