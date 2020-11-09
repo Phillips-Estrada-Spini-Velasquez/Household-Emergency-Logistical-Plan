fc3abe3f635673fbf6138b202cdad33ada69c534
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-11-09 11:32:26 -0600

    ready for deployment with dropped db-2

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -1,5 +1,12 @@
 document.getElementById("invite-email").onclick = function(){
-    alert('Your email invite has been sent.');
-}
+    let person = prompt("Enter an Email Address:");
+    if (person !== null) {
+        txt = "Your email invite has been sent.";
+    }}
+
+
+// document.getElementById("invite-email").onclick = function(){
+//     prompt("Enter an Email Address:");
+// }
 
 
